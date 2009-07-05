a4426c8abcc3bda2a6bd0d039d1ad92c265d2b6c
Travis J Parker <travis.parker@gmail.com>
2009-07-05 16:48:09 -0700

    keep-alive WORKS when i give Content-Length headers in the example

--- examples/wsgiapptest.py
+++ examples/wsgiapptest.py
@@ -8,32 +8,36 @@ import feather
 
 
 HOST = ""
-PORT = 9001
+PORT = 9000
 
 def xss_scrub(data):
     return data.replace("&", "&amp;").replace(">", "&gt;").replace("<", "&lt;")
 
 def wsgiapp(environ, start_response):
     if environ['PATH_INFO'] == '/':
-        start_response("200 OK", [('content-type', 'text/html')])
-        return ['''
+        response = '''
             <form action=/handler method=POST>
                 <input type=text name=data1 /><br>
                 <input type=text name=data2 />
                 <input type=submit />
             </form>
-        ''']
+        '''
+        start_response("200 OK", [('content-type', 'text/html'),
+                                  ('content-length', str(len(response)))])
+        return [response]
     elif environ['PATH_INFO'] == '/handler' and \
             environ['REQUEST_METHOD'] == 'POST':
         data = dict(cgi.parse_qsl(environ['wsgi.input'].read()))
         data['data1'] = xss_scrub(data['data1'])
         data['data2'] = xss_scrub(data['data2'])
-        start_response(200, [('content-type', 'text/html')])
-        return ['''<p>data1 was %(data1)s</p>
+        response = '''<p>data1 was %(data1)s</p>
             <p>data2 was %(data2)s</p>
             <a href="/">back</a>
-        ''' % data]
-    start_response(404, [('content-type', 'text/html')])
+        ''' % data
+        start_response("200 OK", [('content-type', 'text/html'),
+                                  ('content-length', str(len(response)))])
+        return [response]
+    start_response("404 NOT FOUND", [('content-type', 'text/html')])
     return ['nuh-uh']
 
 
