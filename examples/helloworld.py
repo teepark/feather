a85e23e9c661e3536467187fb98fe3602a9c749e
Travis J Parker <travis.parker@gmail.com>
2009-12-28 00:02:29 -0800

    hello, world!

---
+++ examples/helloworld.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import optparse
+
+import feather
+
+
+DEFAULT_HOST = ""
+DEFAULT_PORT = 9000
+
+def wsgiapp(environ, start_response):
+    start_response("200, OK", [
+            ('content-type', 'text/plain'),
+            ('content-length', '13')])
+    return ["Hello, World!"]
+
+
+if __name__ == "__main__":
+    parser = optparse.OptionParser()
+    parser.add_option("-H", "--host", default=DEFAULT_HOST)
+    parser.add_option("-P", "--port", type="int", default=DEFAULT_PORT)
+
+    options, args = parser.parse_args()
+    feather.serve_wsgi_app((options.host, options.port), wsgiapp)
