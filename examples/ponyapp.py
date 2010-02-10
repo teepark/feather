f2a9b74b53df43efff53a1b0b1aa1d9303c2c2de
Travis J Parker <travis.parker@gmail.com>
2010-02-09 17:23:34 -0800

    pony app example fixed for refactored feather code

--- examples/ponyapp.py
+++ examples/ponyapp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import feather
+import feather.wsgi
 from paste.pony import make_pony
 
 
@@ -8,11 +8,11 @@ HOST = ""
 PORT = 9000
 
 def helloworld(environ, start_response):
-    start_response(200, [('content-type', 'text/plain')])
+    start_response("200 OK", [('content-type', 'text/plain')])
     return ["Hello, World!"]
 
 wsgiapp = make_pony(helloworld, None) # 2nd arg just gets thrown away
 
 
 if __name__ == "__main__":
-    feather.serve_wsgi_app((HOST, PORT), wsgiapp)
+    feather.wsgi.serve((HOST, PORT), wsgiapp, traceback_debug=True)
